92dbe0df04aa7dd70c5e46409b9244d78cb75d32
bhoomiagrawal <bhoomika.agrawal@deorwine.com>
2019-08-03 19:54:15 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+
+import logo from "./logo.svg";
+import "./App.css";
+
+
+//By using custom hook
+const useTitle = title => {
+  useEffect(
+    () => {
+      document.title = title;
+    },
+    [title]
+  )
+}
 
 function App() {
+  // Declare a new state variable, which we'll call "count"
+  const [count, setCount] = useState(0);
+ 
+  const [total, setTotal] = useState(5);
+
+  // Similar to componentDidMount and componentDidUpdate:
+  // useEffect(() => {
+  //   // Update the document title using the browser API
+  //   console.log('asasass');
+  //   document.title = `You clicked ${total} times`;
+  //   // return function cleanup() {
+  //   //   setCount(count + 1)
+  //   // };
+  // });
+
+  useEffect(() => {
+    console.log('other effect');
+  });
+
+  const increment  = () => setCount(count + 1);
+  useTitle(count);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>You clicked {count} times</p>
+      <p>useEffect state count (starting with 5): {total}</p>
+      <div>
+      <button onClick={() => {setCount(0); setTotal(5)}} style={{marginLeft: '5px'}}>Reset</button>
+      <button onClick={increment} style={{marginLeft: '5px'}}>+</button>
+      <button onClick={() => setCount(prevCount => prevCount - 1)} style={{marginLeft: '5px'}}>-</button>
+        <button onClick={()=>setTotal(total + 1)} style={{marginLeft: '5px'}}>useEffect state</button>
+      </div>
+      
     </div>
   );
 }
